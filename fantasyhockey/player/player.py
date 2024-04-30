ea28025061ac80a74cad56cd6b2d3b84a83e87b3
Luke Wismer <lukewismer77@gmail.com>
2024-04-30 13:24:12 -0600

    UPdate

--- fantasyhockey/player/player.py
+++
@@ -1,8 +0,0 @@
-from fantasyhockey.api.api_connector import APIConnector
-
-class Player:
-    def __init__(self):
-        pass
-
-    def get_player(self):
-        APIConnector.get("https://api-web.nhle.com")
