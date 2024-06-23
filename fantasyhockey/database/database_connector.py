d25496a3a721f931fdd1d153cb2cb83825537caf
Luke Wismer <lukewismer77@gmail.com>
2024-06-22 17:03:01 -0700

    .

--- fantasyhockey/database/database_connector.py
+++ fantasyhockey/database/database_connector.py
@@ -3,6 +3,7 @@ from mysql.connector import Error
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
 DB_USER  = os.getenv("DB_USER")
 DB_PASSWORD  = os.getenv("DB_PASSWORD")
 DB_HOST  = os.getenv("DB_HOST")
